c14e1e9132a2960f4c6fb623e6f2f155b950a79b
Giorgi226 <giorgi.khakhutashvili26@gmail.com>
2024-12-16 11:34:52 +0400

    minor change in admin controller

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -6,7 +6,6 @@ import { ApiBearerAuth, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger'
 
 @ApiBearerAuth()
 @UseGuards(JwtGuard, RolesGuard)
-@Roles('admin')
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
@@ -16,6 +15,7 @@ export class AdminController {
     @ApiOperation({ summary: 'userebis sia, mati sheqmnis droita da warmatebuli/warumatebeli puzzleebit' })
     @ApiQuery({ name: 'page', required: false, description: 'paginaciis page' })
     @ApiQuery({ name: 'limit', required: false, description: 'paginaciis limiti ert gverdze gantavsebistvis' })
+    @Roles('admin')
     @Get('users')
     getUsers(@Query('page')page: number,@Query('limit')limit: number ){
         return this.adminService.getUsers(page,limit)
